9c0ac8439bb69ebee42b914544dbcf9f29a74f12
Сергей Шмигельский <siro181a@gmail.com>
2020-12-10 23:46:03 +0200

    added first srction

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ $(function () {
         $(".serch__input").toggleClass("header__icons-search--visible");
     });
 })
+
+var mySwiper = new Swiper('.read__container', {
+    speed: 1000,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+})
